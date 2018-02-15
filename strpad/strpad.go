34071c41c5cf8a503a5fd1600d93fb794759d34b
Arvin C. San Andres <arvinsanandres@aveco.com.ph>
2018-02-15 15:16:58 +0800

    added library for string padding

---
+++ strpad/strpad.go
@@ -0,0 +1,19 @@
+package strpad
+
+import "strings"
+
+// Right will return a string padded on the right with 'padStr' to 'overallLen'
+func Right(s string, padStr string, overallLen int) string {
+	var padCountInt int
+	padCountInt = 1 + ((overallLen - len(padStr)) / len(padStr))
+	var retStr = s + strings.Repeat(padStr, padCountInt)
+	return retStr[:overallLen]
+}
+
+// Left will return a string padded on the left with 'padStr' to 'overallLen'
+func Left(s string, padStr string, overallLen int) string {
+	var padCountInt int
+	padCountInt = 1 + ((overallLen - len(padStr)) / len(padStr))
+	var retStr = strings.Repeat(padStr, padCountInt) + s
+	return retStr[(len(retStr) - overallLen):]
+}
